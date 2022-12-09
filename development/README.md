6c6f613b5d7d6552e1890cb0c7d85b51e38e138e
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2022-12-09 15:06:04 +0800

    Add Flask and Vue Template for now

---
+++ development/README.md
@@ -0,0 +1,19 @@
+# Template of Flask Server and Vue.js App
+
+## How to Run
+
+### Server (Flask)
+
+```shell
+$ cd server
+$ pip install -r requirements.txt
+$ python app.py
+```
+
+### Client (Vue.js App)
+
+```shell
+$ cd client
+$ npm i
+$ npm run serve
+```
---
+++ development/client/src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      <v-card-title>
+          Your profile
+        <v-spacer/>
+      </v-card-title>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Main",
+  data: function () {
+    return {
+    };
+  }
+});
+</script>
