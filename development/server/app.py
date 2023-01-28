573b4375f8bb92147a46b41cb5140d3e53fce4bf
RottenLemons <rottenlemons123@gmail.com>
2023-01-28 09:34:26 +0800

    Add verifier in api

--- development/server/app.py
+++ development/server/app.py
@@ -9,6 +9,10 @@ from sgnlp.models.csgec import (
 )
 from sgnlp.models.coherence_momentum import CoherenceMomentumModel, CoherenceMomentumConfig, \
     CoherenceMomentumPreprocessor
+from transformers import pipeline, AutoTokenizer, AutoModel
+import torch
+from sklearn.metrics.pairwise import cosine_similarity
+from keybert import KeyBERT
 
 app = Flask(__name__)
 
@@ -46,6 +50,16 @@ model1 = CoherenceMomentumModel.from_pretrained(
 
 preprocessor1 = CoherenceMomentumPreprocessor(config1.model_size, config1.max_len)
 
+qa_pipeline = pipeline(
+    "question-answering",
+    model="mrm8488/spanbert-finetuned-squadv2",
+    tokenizer="mrm8488/spanbert-finetuned-squadv2"
+)
+
+tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/bert-base-nli-mean-tokens')
+model = AutoModel.from_pretrained('sentence-transformers/bert-base-nli-mean-tokens')
+kw_model = KeyBERT()
+
 @app.route('/grammar', methods=['GET'])
 def grammar():
     text = request.args.get("text")
@@ -63,6 +77,36 @@ def coherence():
 
     return text1_score
 
+@app.route("/answer", methods=["GET"])
+def answer(passage, question):
+    return qa_pipeline({'context': str(passage),'question': str(question)})['answer']
+
+@app.route("/similarity", methods=["GET"])
+def similarity(userText, answerText):
+    userText = kw_model.extract_keywords(userText, keyphrase_ngram_range=(1,1), stop_words='english', use_maxsum=True, nr_candidates=20, top_n=10)
+    userText = ' '.join([x for x, y in userText])
+    answerText = kw_model.extract_keywords(answerText, keyphrase_ngram_range=(1,1), stop_words='english', use_maxsum=True, nr_candidates=20, top_n=10)
+    answerText = ' '.join([x for x, y in answerText])
+    sentences = [userText, answerText]
+    tokens = {'input_ids': [], 'attention_mask': []}
+
+    for sentence in sentences:
+        new_tokens = tokenizer.encode_plus(sentence, max_length=128, truncation=True,padding='max_length', return_tensors='pt')
+        tokens['input_ids'].append(new_tokens['input_ids'][0])
+        tokens['attention_mask'].append(new_tokens['attention_mask'][0])
+
+    tokens['input_ids'] = torch.stack(tokens['input_ids'])
+    tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+    outputs = model(**tokens)
+    embeddings = outputs.last_hidden_state
+    attention_mask = tokens['attention_mask']
+    mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
+    masked_embeddings = embeddings * mask
+    summed = torch.sum(masked_embeddings, 1)
+    summed_mask = torch.clamp(mask.sum(1), min=1e-9)
+    mean_pooled = summed / summed_mask
+    mean_pooled = mean_pooled.detach().numpy()
+    return cosine_similarity([mean_pooled[0]], mean_pooled[1:])[0]
 
 if __name__ == '__main__':
     app.run()
